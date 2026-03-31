b33a4b87ba73d994219474cbbb44efcec4c5d836
soyRosana <rosanagfortes@uma.es>
2026-03-31 12:18:54 +0200

    feat(main): add ideal body weight calculation and fixed height metrics

--- java-project-healthcalc/src/main/java/healthcalc/Main.java
+++ java-project-healthcalc/src/main/java/healthcalc/Main.java
@@ -8,18 +8,21 @@ public class Main {
         try {
             System.out.print("Enter weight (kg): ");
             double p = sc.nextDouble();
-            System.out.print("Enter height (m): ");
+            System.out.print("Enter height (cm): ");
             double a = sc.nextDouble();
-            double imc = calc.bmi(p, a);
+            double imc = calc.bmi(p, a/100.0);
             System.out.println("Your BMI is: " + imc + " (" + calc.bmiClassification(imc) + ")");
+            System.out.print("Enter your gender (H = male, M = female): ");
+            char gender = sc.next().toUpperCase().charAt(0);
+
+            System.out.print("Enter your waist circumference (cm): ");
+            double waist = sc.nextDouble();
+            double ibw = calc.idealBodyWeight(a, gender);
+            System.out.println("Your IBW is: " + ibw);
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-        System.out.print("Enter your gender (H = male, M = female): ");
-        char gender = sc.next().toUpperCase().charAt(0);
-
-        System.out.print("Enter your waist circumference (cm): ");
-        double waist = sc.nextDouble();
+        
     }
 }
 
