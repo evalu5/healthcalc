4678a4d454a8f362782db49d9dd9152d31be2579
soyRosana <rosanagfortes@uma.es>
2026-04-17 22:53:12 +0200

    feat: integrar botón de cálculo y su controlador en PanelIBW

--- java-project-healthcalc/src/main/java/healthcalc/view/PanelIBW.java
+++ java-project-healthcalc/src/main/java/healthcalc/view/PanelIBW.java
@@ -3,13 +3,14 @@ package healthcalc.view;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-
+import java.awt.event.ActionListener;
 
 public class PanelIBW extends JPanel {
 
     private static final long serialVersionUID = 1L;
     private JTextField txtAltura;
     private JTextField txtGenero;
+    private JButton btnCalcular;
 
     public PanelIBW() {
         setLayout(new GridLayout(0, 1, 0, 8));
@@ -21,9 +22,18 @@ public class PanelIBW extends JPanel {
         add(new JLabel("Género (H/M):"));
         txtGenero = new JTextField();
         add(txtGenero);
+        btnCalcular = new JButton("Calcular IBW");
+        btnCalcular.setBackground(new Color(173, 216, 230));
+        add(btnCalcular);
+        add(new JSeparator());
     }
     
     public String getAltura() { return txtAltura.getText(); }
+    
     public String getGenero() { return txtGenero.getText(); }
     
+    public void setBtnController(ActionListener ctr) {
+        btnCalcular.addActionListener(ctr);
+        btnCalcular.setActionCommand("CALCULAR_IBW");
+    }
 }
